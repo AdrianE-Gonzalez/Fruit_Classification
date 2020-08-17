810d28232e35ff1b822ae0319ac5cca65ad8c51f
AdrianE-Gonzalez <adriangonzalez8@yahoo.com>
2020-08-17 12:50:38 -0500

    Wrote The Code For Main.py To  Use Both Load_data.py And NN.py; Added Plot_Data To Visually See Accuracies; Added Comments

--- Fruit_ML_Code/Main.py
+++ Fruit_ML_Code/Main.py
@@ -1,4 +1,38 @@
 import load_data
+import matplotlib.pyplot as plt
 import nn
-#Main Python File For Code
+#Main Python File Where It Uses All The Files Created To Run Code
+#Planned Algorithms NN, Decision Tree,...
+
+#Plots Accuracies For Model Accuracy And Model Loss
+def plot_data(hist):
+    #summarize history for accuracy
+    plt.plot(hist.history['accuracy'])
+    plt.plot(hist.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train','test'], loc='upper left')
+
+    plot.show()
+
+    #summarize history for loss
+    plt.plot(hist.history['loss'])
+    plt.plot(hist.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+
+    plt.show()
+
 print("Main Code")
+train_data,val_data,test_data= load_data.load_datasets()
+
+hist = nn.run_model(train_data,val_data)
+
+plot_data(hist)
+#print(train_data.class_names)
+#print(val_data.class_names)
+
+print("Done")
